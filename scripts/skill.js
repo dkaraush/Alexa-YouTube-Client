1a37f9cc90f921a81855fce71cef5dde20db3e27
Dima Karaush <dkaraush@gmail.com>
2018-07-30 19:14:39 +0300

    test

--- scripts/skill.js
+++ scripts/skill.js
@@ -915,5 +915,6 @@ function thumbnail(snippet) {
 function redirectVideo(link) {
 	var id = randomString(16);
 	redirects[id] = link;
-	return config.server_url+"/"+id+".mp4";
+	return "http://techslides.com/demos/sample-videos/small.mp4";
+	//return config.server_url+"/"+id+".mp4";
 }
