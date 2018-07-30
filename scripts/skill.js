ab4402a6cd23852dde72c4c540ef6a49bd88b8c2
Dima Karaush <dkaraush@gmail.com>
2018-07-30 18:04:30 +0300

    [test] always own videos url

--- scripts/skill.js
+++ scripts/skill.js
@@ -693,7 +693,7 @@ async function runVideo(RI, requestname, data, cantalk, behavior, type, youtube,
 			link = config.server_url + "/videos?" + encodeURIComponent(link);
 		
 		if (type)
-			return res.addVideoAppLaunchDirective(link);
+			return res.addVideoAppLaunchDirective(config.server_url + "/videos?" + encodeURIComponent(link));
 		return res.addAudioPlayerPlayDirective(behavior, link, videoId, offset, behavior == "ENQUEUE" ? waslasttoken : null, metadata(data.pitems[data.index]));
 	}
 	return new Promise((resolve, reject) => {
@@ -711,7 +711,7 @@ async function runVideo(RI, requestname, data, cantalk, behavior, type, youtube,
 				}
 
 				if (type)
-					resolve(res.addVideoAppLaunchDirective(link));
+					resolve(res.addVideoAppLaunchDirective(config.server_url + "/videos?" + encodeURIComponent(link)));
 				else resolve(res.addAudioPlayerPlayDirective(behavior, link, videoId, offset, behavior == "ENQUEUE" ? waslasttoken : null, metadata(data.pitems[data.index])));
 			})
 			.catch(e => {
