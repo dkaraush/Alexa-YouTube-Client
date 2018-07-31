291ce5ad6bf8da52fc8b11346c6ee1d93f8928ef
Dima Karaush <dkaraush@gmail.com>
2018-07-31 18:15:49 +0300

    ~

--- scripts/skill.js
+++ scripts/skill.js
@@ -634,7 +634,7 @@ var errorHandler = {
 async function runVideo(RI, requestname, data, cantalk, behavior, type, youtube, user, res, speech) {
 	if (data.index == 0 && type) {
 		return new Promise((resolve, reject) => {
-			var playlist = data.pitems.slice(0, 5);
+			var playlist = data.pitems.slice(0, 2);
 			playlist.forEachEnd((video, cb) => {
 				youtubedl(getID(RI, video), type, RI)
 					.then(async function(link) {
@@ -644,6 +644,7 @@ async function runVideo(RI, requestname, data, cantalk, behavior, type, youtube,
 							return;
 						}
 						res = res.addVideoAppLaunchDirective(plink);
+						cb();
 					})
 			}, () => {
 				resolve(res);
